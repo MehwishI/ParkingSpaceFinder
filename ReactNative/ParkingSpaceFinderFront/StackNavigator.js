9fbba3d5f25df4761e8d19514107c016673513d0
MehwishI <mehwishned21@gmail.com>
2024-09-15 13:39:36 -0500

    Adding Home page

---
+++ ReactNative/ParkingSpaceFinderFront/StackNavigator.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+//import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from './screens/Home';
+
+//const Stack = createNativeStackNavigator();
+const StackNavigator = () => {
+  return (
+    
+    <Stack.Navigator screenOptions={{ headerShown: false }} >
+      <Stack.Group>
+        <Stack.Screen name="Home" component= {Home}/>
+    </Stack.Group>
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator
---
+++ ReactNative/ParkingSpaceFinderFront/screens/Home.js
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to the Home page of ParkSmart app</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Home
