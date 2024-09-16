5721b61af2dd5fb30c46d08b997369f7d48877fa
MehwishI <mehwishned21@gmail.com>
2024-09-15 23:02:15 -0500

    node upgrade

--- ReactNative/ParkingSpaceFinderFront/StackNavigator.js
+++ ReactNative/ParkingSpaceFinderFront/StackNavigator.js
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-//import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from './screens/Home';
 
 //const Stack = createNativeStackNavigator();
