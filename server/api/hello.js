689587c48e09182e0fcb10288f8fe2e9986229b5
Adekunle <kunleoshodi856@gmail.com>
2024-09-17 08:16:28 -0500

    Implemented swagger UI

--- server/api/hello.js
+++ server/api/hello.js
@@ -1,3 +1,21 @@
+/**
+ * @swagger
+ * /api/hello:
+ *   get:
+ *     summary: Returns a Hello message
+ *     responses:
+ *       200:
+ *         description: A successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Hello from the backend!
+ */
+
 module.exports = (req, res) => {
   res.status(200).json({ message: "Hello from the backend!" });
 };
--- server/server.js
+++ server/server.js
@@ -7,6 +7,29 @@ const app = express();
 //const PORT = 3000;
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Parking Space Finder',
+            version: '1.0.0',
+            description: 'A web API with Swagger documentation for the parking space finder application',
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000',
+            },
+        ],
+    },
+    apis: ['./api/*.js'],
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.set('view engine', 'ejs');
 app.use(morgan('dev'));
@@ -14,20 +37,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.status(200);
     res.send("Welcome to root URL of Server");
 });
-app.get('/index', (req, res)=>{
+app.get('/index', (req, res) => {
     res.status(200);
     res.render("index");
 });
 
 
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
-    else 
+app.listen(PORT, (error) => {
+    if (!error)
+        console.log("Server is Successfully Running, and App is listening on port " + PORT)
+    else
         console.log("Error occurred, server can't start", error);
-    }
+}
 );
