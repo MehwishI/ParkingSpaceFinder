87be2b9b02892b7fc7c7da5534ccc98152322fb5
Mehwish <mehwishned21@gmail.com>
2024-09-28 19:49:42 -0500

    Integrate Google Map

--- ReactJS/parking-spot-finder/src/components/MapContainer/MapContainer.jsx
+++ ReactJS/parking-spot-finder/src/components/MapContainer/MapContainer.jsx
@@ -1,58 +1,72 @@
 import React from 'react';
-import { GoogleMap, LoadScript, useJsApiLoader } from '@react-google-maps/api';
+import { useMemo, useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, useJsApiLoader ,MarkerF} from '@react-google-maps/api';
 import './MapContainer.css'
 
-const  MapContainer = () => {
+const MapContainer = () => {
+  
+   var initialCenter = { lat: 48.1, lng: -97.39 }
+  const [defaultCenter, setDefaultCenter ] = useState(initialCenter)
 
   const { isLoaded } = useJsApiLoader({
-     id: 'google-map-script',
+    id: 'google-map-script',
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-   });
+  });
   
   const mapStyles = {
-    height: "700px",
-    width: "800px",
+    height: "100%",
+    width: "100%",
     position: "relative",
     overflow: "none",
-   align : "center"
-   // justify-content: "center"
-  };
-// var userLocation ={}
-  var defaultCenter = {
-    lat: 49.3851, lng: -97.1734
+    align: "center"
+    // justify-content: "center"
   };
  
- if (!isLoaded) {
+  useEffect(() => {
+       
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude;
+        const long = position.coords.longitude;
+        // const center = { lat: lat, lng: long };
+        setDefaultCenter({ lat: latitude, lng: long });
+      },
+      (error) => {
+        console.log(error);
+      })
+  },[])
+    
+  // const showPosition = (position) => {
+  //   console.log(position)
+  //  // console.log(userLocation)
+  //   const userLocation = {            
+  //         lat: position.coords.latitude,  
+  //         lng: position.coords.longitude, 
+  //   };
+  //   defaultCenter.lat = userLocation.lat;
+  //   defaultCenter.lng = userLocation.lng;
+  //   console.log("changed defaultcenter:",defaultCenter)
+  // }  
+ // getUserLocation()
+  console.log("defaultcenter after getting user location:", defaultCenter)
+  if (!isLoaded) {
     return (
       <div>
         <p>Loading...</p>
       </div>
     );
   }
-  const getUserLocation = () => {
-    // console.log(navigator.geolocation)
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    }
-    else {
-      return null;
-    }
-  }
-  const showPosition = (position) => {
-    console.log(position)
-   // console.log(userLocation)
-    const userLocation = {            
-          lat: position.coords.latitude,  
-          lng: position.coords.longitude, 
-    };
-    defaultCenter.lat = userLocation.lat;
-    defaultCenter.lng = userLocation.lng;
-    console.log(defaultCenter)
-  }  
-  
-  
  
-getUserLocation()
+  //{ defaultCenter.lat, defaultCenter.lng } = getUserLocation();
+  
+//   const renderMarkers = (map, maps) => {
+//   let marker = new maps.Marker({
+//   position: { lat: defaultCenter.lat, lng: defaultCenter.lng },
+//   map,
+//   title: 'Hello World!'
+//   });
+//   return marker;
+//  };
 
   return (
     // <LoadScript
@@ -60,10 +74,15 @@ getUserLocation()
     <div className='mapcontainer'>  
     <GoogleMap
         mapContainerStyle={mapStyles}
-        zoom={13}
-        center={defaultCenter} />
-      </div>
-    //</LoadScript>
+        zoom={10}
+        center={defaultCenter}
+       // onGoogleApiLoaded={renderMarkers(map,maps)}
+      >
+        <MarkerF key="current_location"  title="You are here!"  animation= "DROP" position={{ lat: defaultCenter.lat, lng: -97.1419 }}  />
+        </GoogleMap>
+      
+    </div>
+   
   );
 }
 export default MapContainer
