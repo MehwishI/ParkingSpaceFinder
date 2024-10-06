ef4013d86887feb709048231aa2fa7e53e589497
Mehwish <mehwishned21@gmail.com>
2024-10-06 15:20:35 -0500

    Add user data api endpoints

--- server/models/UserParking.js
+++ server/models/UserParking.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const UserParkingSchema = new mongoose.Schema({
   userid: { type: Number, required: true },
-  locLatitude: { type: mongoose.Types.Decimal128, required: true },
-  locLongitude: { type: mongoose.Types.Decimal128, required: true },
-  date: { type: Date, required: true },
+  parkhistory: {
+    locLatitude: { type: mongoose.Types.Decimal128, required: true },
+    locLongitude: { type: mongoose.Types.Decimal128, required: true },
+    date: { type: Date, default: Date.now },
+  },
 });
 
 module.exports = mongoose.model("UserParking", UserParkingSchema);
--- server/services/userDataService.js
+++ server/services/userDataService.js
@@ -1,15 +1,38 @@
-const userData = require('./models/User.js')
+const userCollection = require("../models/User.js");
 
-import axios from"axios";
+//import axios from "axios";
 
 //get user data from db
 //call encrypt data() --sensitive data
+const getUserData = async (userId) => {
+  try {
+    const parkdata = await userCollection
+      .find(
+        {
+          userid: userId,
+        },
+        userProfile
+      )
+      .exec();
+    return userProfile;
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 // post user data to db
 //call encrypt data() --sensitive data
+const saveUserData = async (userData) => {
+  console.log("userData in service:", userData);
+  console.log("reached user data service");
+  try {
+    const newUser = new userCollection(userData);
+    const savedUser = await newUser.save();
+    console.log("User saved: ", savedUser);
+    return savedUser;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-//get user parking data from db
-//call encrypt data()
-
-//post user parking data to db
-//call encrypt data()
+module.exports = { getUserData, saveUserData };
