2d2b0dee8db6af9dfcd1079187d9e8cd53e20979
Adekunle <kunleoshodi856@gmail.com>
2024-10-28 16:21:13 -0500

    Fixed bug

--- ReactJS/parking-spot-finder/src/components/LoaderContainer/Loader.js
+++ ReactJS/parking-spot-finder/src/components/LoaderContainer/Loader.js
@@ -1,43 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router';
+import React from 'react';
 import './Loader.css';
-import { useAuth0 } from '@auth0/auth0-react';
-// import logo from './samplelogo.png';
 
 const Loader = () => {
-    const [loading, setLoading] = useState(true);
-    // const navigate = useNavigate();
-    const { isAuthenticated } = useAuth0();
-
-    useEffect(() => {
-        setTimeout(() => {
-            console.log("is authenticated",isAuthenticated);
-            
-            setLoading(false);
-            
-            if (isAuthenticated) {
-                //navigate('/home');
-            } else {
-                // navigate('/home');
-                // navigate('/login');
-            }
-        }, 3000);
-    }, []);
-// }, [navigate]);
-
     return (
         <div className='logo-load'>
-            {loading && (
-                <div className='logo-contain'>
-                    <img
-                        // src={logo}
-                        src=''
-                        alt="SmartPark Logo"
-                        className="shine-logo"
-                    />
-                    {/* <div className="shine-effect"></div> */}
-                </div>
-            )}
+            <div className='logo-contain'>
+                <img
+                    src=''
+                    alt="SmartPark Logo"
+                    className="shine-logo"
+                />
+            </div>
         </div>
     );
 };
